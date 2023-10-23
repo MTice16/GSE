131862d1b15cf0c1ada0905386218404b2dd05e6
MTice16 <148655139+MTice16@users.noreply.github.com>
2023-10-23 19:49:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
+        const wheelContainer = document.querySelector('.wheel-container');
+        const animationGraphic = document.getElementById('animationGraphic');
+        const spinButton = document.getElementById('spinButton');
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
-}
+        spinButton.addEventListener('click', () => {
+            // Disable the button during the animation
+            spinButton.disabled = true;
+
+            // Start the spinning animation
+            wheelContainer.style.animation = 'spin 10s linear forwards';
+
+            // Show the animation graphic after 10 seconds
+            setTimeout(() => {
+                wheelContainer.style.animation = 'none'; // Stop the wheel animation
+                animationGraphic.style.display = 'block';
+                spinButton.disabled = false; // Re-enable the button
+            }, 10000); // 10 seconds (10000 milliseconds)
+        });
+    </script>
